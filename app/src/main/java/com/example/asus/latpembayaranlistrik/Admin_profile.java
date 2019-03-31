160bd0b74fea85ef57cdc642981df6831d9c429d
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2019-04-01 03:37:17 +0700

    Fix All

---
+++ app/src/main/java/com/example/asus/latpembayaranlistrik/Admin_profile.java
@@ -0,0 +1,13 @@
+package com.example.asus.latpembayaranlistrik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Admin_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_profile);
+    }
+}
--- app/src/main/java/com/example/asus/latpembayaranlistrik/Main_Admin.java
+++ app/src/main/java/com/example/asus/latpembayaranlistrik/Main_Admin.java
@@ -1,6 +1,8 @@
 package com.example.asus.latpembayaranlistrik;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,8 +15,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class Main_Admin extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private long backPressedTime;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private String userID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +44,25 @@ public class Main_Admin extends AppCompatActivity
         setContentView(R.layout.activity_main_admin);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference().child("ShaMind").child("Room");
+        FirebaseUser user = mAuth.getCurrentUser();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is Signed In
+                } else {
+                    // User is signed out
+                    Intent mIntent = new Intent(Main_Admin.this, Login.class);
+                    startActivity(mIntent);
+                }
+            }
+        };
+        userID = user.getUid();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -91,7 +132,9 @@ public class Main_Admin extends AppCompatActivity
         } else if (id == R.id.nav_verifikasi_validasi) {
 
         } else if (id == R.id.nav_logout) {
-
+            mAuth.signOut();
+            Intent mIntent = new Intent(Main_Admin.this, Login.class);
+            startActivity(mIntent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
